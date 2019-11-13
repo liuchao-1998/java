8e438c6fc867f3de384086c5c4e0d7fdc6554d56
liuchao-1998 <921273205@qq.com>
2019-11-13 19:47:37 +0800

    tijiao

---
+++ ssm/src/main/java/cn/xdl/controller/XdlBankAccountController.java
@@ -0,0 +1,36 @@
+package cn.xdl.controller;
+
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import cn.xdl.bean.XdlBankAccount;
+import cn.xdl.service.XdlBankAccountService;
+
+@Controller
+public class XdlBankAccountController {
+	@Autowired
+	private  XdlBankAccountService  bankService;
+	@RequestMapping("/toLogin.do")
+    public  String  toLogin() {
+    	return "login";
+    }
+	@RequestMapping("/toMain.do")
+    public  String  toMain() {
+    	return "main";
+    }
+	@RequestMapping("/login.do")
+	public String  login(String acc_no,String acc_password,HttpServletRequest request) {
+		XdlBankAccount  account = bankService.login(acc_no, acc_password);
+		if(account != null) {
+		    request.getSession().setAttribute("account", account);
+		    return  "redirect:toMain.do";
+		}
+		request.setAttribute("msg", "鐧诲綍澶辫触");
+		return  "login";
+	}
+}
